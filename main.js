654b7355fbfccf38f66182288b27a9b36b37fe37
joshuamakula <makulajosh@gmail.com>
2021-03-09 13:24:11 +0300

    added media queries

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-// open toggle for humberger menu
+ // open toggle for humberger menu
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('top-nav .nav-link').on('click', function () {
+        $('top-nav .nav-link').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
 });
+
+
