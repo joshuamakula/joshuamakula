5a81f50c7bd0936f0466837bc864c1d9c92dc53f
joshuamakula <makulajosh@gmail.com>
2021-03-09 09:21:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// open toggle for humberger menu
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+});
